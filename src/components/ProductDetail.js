d15f872087c862d076695dd17ecd813a6975f3c4
Julian-regino <julianregino862@gmail.com>
2024-10-07 18:59:11 -0500

    commit11

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -4,3 +4,11 @@ const ProductDetail = ({ product, closeDetail }) => {
     return (
         <div className="product-detail">
             <img src={product.image} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>Precio: ${product.price}</p>
+            <button onClick={closeDetail}>Cerrar</button>
+        </div>
+    );
+};
+
+export default ProductDetail;
