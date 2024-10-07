dc7273c59adcd95eef327b8424d642ddbc19c0a2
Julian-regino <julianregino862@gmail.com>
2024-10-07 18:57:09 -0500

    commit7

---
+++ src/components/CartSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartSummary = ({ cartItems }) => {
+    const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+    return (
+        <div className="cart-summary">
+            <h2>Resumen del Carrito</h2>
+            <p>Total de productos: {cartItems.length}</p>
+            <p>Total: ${totalPrice}</p>
+        </div>
+    );
+};
