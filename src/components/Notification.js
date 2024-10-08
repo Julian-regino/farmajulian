be94939597c8505fb59bcd5d41c22ba412e780a0
Julian-regino <julianregino862@gmail.com>
2024-10-07 19:01:19 -0500

    commit15

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -3,3 +3,10 @@ import React from 'react';
 const Notification = ({ message, closeNotification }) => {
     return (
         <div className="notification"></div>
+        <p>{message}</p>
+        <button onClick={closeNotification}>Cerrar</button>
+    </div>
+);
+};
+
+export default Notification;
